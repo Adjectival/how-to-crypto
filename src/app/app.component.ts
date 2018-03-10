3d9c6e2557c1db70efa68d5ea5c9f92e47730d83
Adjectival <alexd.jacks@gmail.com>
2018-03-10 13:05:55 -0800

    :wrench: adds rough carousel for Trade path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,12 @@ import { Component } from '@angular/core';
 
   <ul class="nav nav-pills nav-justified">
     <li class="nav-item">
-      <a class="nav-link active" href="#!" id="yesplz">
+      <a class="nav-link" href="#!">
             Buy
       </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#!">Trade</a>
+      <a class="nav-link active" href="#!">Trade</a>
     </li>
     <li class="nav-item">
       <a class="nav-link" href="#!">Mine</a>
@@ -25,6 +25,64 @@ import { Component } from '@angular/core';
 
 
 <!--Body-->
+
+
+
+<!--Carousel Wrapper-->
+<mdb-carousel [isControls]="true" class="carousel slide carousel-fade" [animation]="'fade'">
+    <!--First slide-->
+    <mdb-slide>
+        <div class="view hm-black-strong">
+            <img class="d-block w-75 mx-auto" src="../assets/trade0.png" alt="First slide">
+            <div class="mask waves-light" mdbRippleRadius></div>
+        </div>
+        <div class="carousel-caption">
+            <h3 class="h3-responsive">Trade crypto</h3>
+            <p>Choose Exchange Advanced for best options</p>
+        </div>
+    </mdb-slide>
+    <!--/First slide-->
+    <!--Second slide-->
+    <mdb-slide>
+        <div class="view hm-black-strong">
+            <img class="d-block w-50 mx-auto" src="../assets/trade2.png" alt="Second slide">
+            <div class="mask waves-light" mdbRippleRadius></div>
+        </div>
+        <div class="carousel-caption">
+            <h3 class="h3-responsive">Exchange Board</h3>
+            <p>Just focus on these areas for now.</p>
+            <p>On top you choose the exchange you want. Maybe it's buying NEO using ETHereum.</p>
+        </div>
+    </mdb-slide>
+    <!--/Second slide-->
+    <!--Third slide-->
+    <mdb-slide>
+        <div class="view hm-black-strong">
+            <img class="d-block w-50 mx-auto" src="../assets/trade1.png" alt="Third slide">
+            <div class="mask waves-light" mdbRippleRadius></div>
+        </div>
+        <div class="carousel-caption">
+            <h3 class="h3-responsive">Bottom right corner</h3>
+            <p>Where you exchange</p>
+        </div>
+    </mdb-slide>
+    <!--/Third slide-->
+    <!--Fourth slide-->
+    <mdb-slide>
+        <div class="view hm-black-slight">
+            <img class="d-block w-75 mx-auto" src="../assets/trade3.png" alt="Fourth slide">
+            <div class="mask waves-light" mdbRippleRadius></div>
+        </div>
+        <div class="carousel-caption">
+            <h3 class="h3-responsive">Choose 'Market'</h3>
+            <p>You can use the 25%, 50%, 100% buttons to see how much you'll buy.</p>
+        </div>
+    </mdb-slide>
+    <!--/Third slide-->
+</mdb-carousel>
+<!--/.Carousel Wrapper-->
+
+
 
 <!--Card1-->
 <div class="col-md-4 my-3 text mx-auto">
@@ -93,7 +151,6 @@ import { Component } from '@angular/core';
     </div>
 </div>
 <!--/.Card2-->
-
 
 
 
