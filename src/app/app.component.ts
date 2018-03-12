5c982710d786a2ebe7ac7c8a5a24ec09f83d6153
Adjectival <alexd.jacks@gmail.com>
2018-03-12 15:06:06 -0700

    :art::wrench: improves separation in user paths and on nav; builds out rough templates for Trade path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,156 +7,40 @@ import { Component } from '@angular/core';
 
   <ul class="nav nav-pills nav-justified">
     <li class="nav-item">
-      <a class="nav-link" href="https://colors-crypto.firebaseapp.com">
+        <a class="nav-link" href="https://colors-crypto.firebaseapp.com">
+            <i class="fas fa-th-large"></i>
             Prices
-      </a>
+        </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" routerLink="buy">
+        <a class="nav-link" routerLink="buy">
+
             Buy
-      </a>
+        </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link active" routerLink="trade">Trade</a>
+        <a class="nav-link" routerLink="trade">
+
+            Trade
+        </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" routerLink="mine">Mine</a>
+      <a class="nav-link" routerLink="mine">
+
+            Mine
+        </a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="mailto:alexd.jacks@gmail.com">Email Me!</a>
+      <a class="nav-link" href="mailto:alexd.jacks@gmail.com">
+            <i class="fas fa-paper-plane"></i>
+            Help?
+        </a>
     </li>
   </ul>
 <router-outlet></router-outlet>
 
 
 <!--Body-->
-
-
-
-<!--Carousel Wrapper-->
-<mdb-carousel [isControls]="true" class="carousel slide carousel-fade" [animation]="'fade'">
-    <!--First slide-->
-    <mdb-slide>
-        <div class="view hm-black-strong">
-            <img class="d-block w-100 mx-auto" src="../assets/trade0.png" alt="First slide">
-            <div class="mask waves-light" mdbRippleRadius></div>
-        </div>
-        <div class="carousel-caption">
-            <h3 class="h3-responsive">Trade crypto</h3>
-            <p>Choose Exchange Advanced for best options</p>
-        </div>
-    </mdb-slide>
-    <!--/First slide-->
-    <!--Second slide-->
-    <mdb-slide>
-        <div class="view hm-black-strong">
-            <img class="d-block w-50 mx-auto" src="../assets/trade2.png" alt="Second slide">
-            <div class="mask waves-light" mdbRippleRadius></div>
-        </div>
-        <div class="carousel-caption">
-            <h3 class="h3-responsive">Exchange Board</h3>
-            <p>Just focus on these areas for now.</p>
-            <p>On top you choose the exchange you want. Maybe it's buying NEO using ETHereum.</p>
-        </div>
-    </mdb-slide>
-    <!--/Second slide-->
-    <!--Third slide-->
-    <mdb-slide>
-        <div class="view hm-black-strong">
-            <img class="d-block w-50 mx-auto" src="../assets/trade1.png" alt="Third slide">
-            <div class="mask waves-light" mdbRippleRadius></div>
-        </div>
-        <div class="carousel-caption">
-            <h3 class="h3-responsive">Bottom right corner</h3>
-            <p>Where you exchange</p>
-        </div>
-    </mdb-slide>
-    <!--/Third slide-->
-    <!--Fourth slide-->
-    <mdb-slide>
-        <div class="view hm-black-strong">
-            <img class="d-block w-100 mx-auto" src="../assets/trade3.png" alt="Fourth slide">
-            <div class="mask waves-light" mdbRippleRadius></div>
-        </div>
-        <div class="carousel-caption">
-            <h3 class="h3-responsive">Choose 'Market'</h3>
-            <p>You can use the 25%, 50%, 100% buttons to see how much you'll buy.</p>
-        </div>
-    </mdb-slide>
-    <!--/Third slide-->
-</mdb-carousel>
-<!--/.Carousel Wrapper-->
-
-
-
-<!--Card1-->
-<div class="col-md-4 my-3 text mx-auto">
-
-    <!--Card-->
-    <div class="card card-cascade narrower">
-
-        <!--Card image-->
-        <div class="view overlay hm-white-slight waves-light" mdbRippleRadius>
-            <img src="../assets/me_on_kitchen.jpg" class="img-fluid" alt="">
-            <a>
-                <div class="mask"></div>
-            </a>
-        </div>
-        <!--/.Card image-->
-
-        <!--Card content-->
-        <div class="card-body">
-            <h5 class="pink-text">Crypto</h5>
-            <!--Title-->
-            <h4 class="card-title">Buy Crypto</h4>
-            <!--Text-->
-            <p class="card-text">
-                If price lower than usual, today is a good day to buy crypto!!
-            </p>
-            <p>
-                If price within 15% of all-time high, SELL SELL SELL
-            </p>
-            <a class="btn btn-unique waves-light" mdbRippleRadius>BUY</a>
-        </div>
-        <!--/.Card content-->
-
-    </div>
-
-</div>
-<!--/.Card1-->
-
-
-<!--Card2-->
-<div class="col-md-4 my-3 text mx-auto">
-    <!--Card-->
-    <div class="card card-cascade narrower">
-        <!--Card image-->
-        <div class="view overlay hm-white-slight waves-light" mdbRippleRadius>
-            <img src="../assets/me_on_kitchen.jpg" class="img-fluid" alt="">
-            <a>
-                <div class="mask"></div>
-            </a>
-        </div>
-        <!--/.Card image-->
-        <!--Card content-->
-        <div class="card-body">
-            <h5 class="pink-text">Crypto</h5>
-            <!--Title-->
-            <h4 class="card-title">Trade Crypto</h4>
-            <!--Text-->
-            <p class="card-text">
-                Use Binance to exchange crypto
-            </p>
-            <p>
-                Will you speculate quickly, or HODL?
-            </p>
-            <a class="btn btn-unique waves-light" mdbRippleRadius>TRADE</a>
-        </div>
-        <!--/.Card content-->
-    </div>
-</div>
-<!--/.Card2-->
-
 
 
 <!--/Body-->
