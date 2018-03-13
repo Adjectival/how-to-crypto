e97470d327cfbec8e05cd7d2e5932a09fb1d5971
Adjectival <alexd.jacks@gmail.com>
2018-03-12 23:14:09 -0700

    :art: cleans up landing page, perhaps best it will get w/o more time wasted on Ng

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,14 +4,16 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   styleUrls: ['./home.component.scss'],
   template: `
-  <h1>How To Crypto</h1>
-  <h2>A simple guide to start out in a complex new money system</h2>
-  <h3>Questions? Use the navigation bar up top.</h3>
-  <p>
-  Extensive questions?<br/>
-    <i class="fas fa-paper-plane"></i>
-    <a href="mailto:alexd.jacks@gmail.com">Email the author.</a>
-  </p>
+  <div class="centred m-5 card p-4">
+      <h1>How To Crypto</h1>
+      <h2>A simple guide to start out in a complex new money system</h2>
+      <h3>Questions? Use the navigation bar up top.</h3>
+      <p>
+      Extensive questions?<br/>
+        <i class="fas fa-paper-plane"></i>
+        <a href="mailto:alexd.jacks@gmail.com">Email the author.</a>
+      </p>
+  </div>
   `
 })
 export class HomeComponent implements OnInit {
