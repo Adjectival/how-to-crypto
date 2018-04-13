4ded40a837f9f4cb050080bdc02145e79c4885fe
Adjectival <alexd.jacks@gmail.com>
2018-04-13 15:47:54 -0700

    :construction: adapting navbar to include more content & responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,44 +4,96 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   styleUrls: ['./app.component.scss'],
   template: `
-    <!--Body-->
+<!--Body-->
+    <!--Navbar-->
+        <mdb-navbar SideClass="navbar navbar-expand-lg navbar-dark indigo">
 
-    <ul class="nav nav-pills nav-justified fixed-top">
-      <li class="nav-item">
-          <a class="nav-link" routerLink="home">
-          <i class="far fa-question-circle"></i>
-            What?
-          </a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link" routerLink="buy">
-            <i class="far fa-money-bill-alt"></i>
-             Buy
-          </a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link" routerLink="trade">
-            <i class="fas fa-recycle"></i>
-            Trade
-          </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" routerLink="mine">
-            <i class="fab fa-monero"></i>
-            Mine
-          </a>
-      </li>
-      <li class="nav-item">
-          <a class="nav-link" href="https://colors-crypto.firebaseapp.com">
-            <i class="fas fa-th-large"></i>
-            Prices
-          </a>
-      </li>
-    </ul>
+        <!-- Navbar brand -->
+        <logo><a class="navbar-brand" href="#">HowToCrypto</a></logo>
 
-    <router-outlet>
-    </router-outlet>
-    <!--/Body-->
+        <!-- Collapsible content -->
+        <links>
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+
+              <a class="nav-link waves-light" mdbRippleRadius routerLink="home">
+              <span class="sr-only">(current)</span>
+              <i class="far fa-question-circle"></i>
+                What?
+              </a>
+          </li>
+          <li class="nav-item">
+
+              <a class="nav-link waves-light" mdbRippleRadius routerLink="buy">
+
+                <i class="far fa-money-bill-alt"></i>
+                 Buy
+              </a>
+          </li>
+          <li class="nav-item">
+
+              <a class="nav-link waves-light" mdbRippleRadius routerLink="trade">
+
+                <i class="fas fa-recycle"></i>
+                Trade
+              </a>
+          </li>
+          <li class="nav-item">
+
+            <a class="nav-link waves-light" mdbRippleRadius routerLink="mine">
+
+                <i class="fab fa-monero"></i>
+                Mine
+              </a>
+          </li>
+          <li class="nav-item">
+
+              <a class="nav-link waves-light" mdbRippleRadius href="https://colors-crypto.firebaseapp.com">
+
+                <i class="fas fa-th-large"></i>
+                Prices
+              </a>
+          </li>
+
+
+
+        <!-- Dropdown -->
+          <li class="nav-item dropdown" dropdown>
+            <a dropdownToggle mdbRippleRadius type="button" class="nav-link dropdown-toggle waves-light" mdbRippleRadius>
+                <i class="fas fa-th-large"></i>
+                Mining Tools
+                <span class="caret"></span>
+            </a>
+              <div *dropdownMenu class="dropdown-menu dropdown dropdown-primary" role="menu">
+                <a class="dropdown-item waves-light"
+                mdbRippleRadius
+                href="http://www.mycryptobuddy.com/GPUProfitability">
+                    GPU Calculator
+                </a>
+                <a class="dropdown-item waves-light"
+                mdbRippleRadius
+                href="https://www.cryptocompare.com/mining/calculator/eth?HashingPower=110&HashingUnit=MH%2Fs&PowerConsumption=600&CostPerkWh=0.12&MiningPoolFee=1">
+                    Electric Calculator
+                </a>
+                <div class="divider dropdown-divider"></div>
+                <a class="dropdown-item waves-light"
+                mdbRippleRadius
+                href="https://www.reddit.com/r/cryptomining/
+                ">
+                    /r/CryptoMining
+                </a>
+              </div>
+          </li>
+
+        </ul>
+
+        </links>
+       <!-- Collapsible content -->
+
+    </mdb-navbar>
+    <!--/.Navbar-->
+
+<!--/Body-->
   `
 })
 export class AppComponent {
